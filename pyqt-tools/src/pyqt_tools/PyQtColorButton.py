f6f474ec50e8739bfc22cbb0084f5a1b39d87970
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2023-12-22 14:56:33 -0600

    init package pyqt-tools

---
+++ pyqt-tools/src/pyqt_tools/PyQtColorButton.py
@@ -0,0 +1,42 @@
+""" PySide/PyQt button for selecting and displaying a color.
+"""
+
+
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
+
+
+class ColorButton(QToolButton):
+    """ Button for selecting and displaying a color.
+    """
+    
+    def __init__(self, color = QColor('transparent')):
+        QToolButton.__init__(self)
+        self.setColor(color)
+        self.clicked.connect(self.pickColor)
+    
+    def color(self) -> QColor:
+        return self._color
+
+    def setColor(self, color: QColor):
+        self.setStyleSheet(f'background-color: rgba({color.red()}, {color.green()}, {color.blue()}, {color.alpha()}); border: 1px solid black;')
+        self._color = color
+    
+    def pickColor(self):
+        color = QColorDialog.getColor(self.color(), None, "Select Color", options=QColorDialog.ShowAlphaChannel)
+        if color.isValid():
+            self.setColor(color)
+
+
+def test_live():
+    import sys
+    app = QApplication(sys.argv)
+    ui = ColorButton()
+    ui.show()
+    status = app.exec()
+    sys.exit(status)
+
+
+if __name__ == '__main__':
+    test_live()
